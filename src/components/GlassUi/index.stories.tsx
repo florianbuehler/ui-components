d6d3b69b4f69dd0315dbf55fc8987576296f42ce
Florian Bühler <florian-buehler@outlook.com>
2021-03-06 19:56:35 +0100

    feat: Add frame and story for GlassUi.

---
+++ src/components/GlassUi/index.stories.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import GlassUi from './index'
+
+export default {
+  title: 'Components/GlassUi',
+  component: GlassUi
+} as Meta
+
+export const GlassUiStory: Story = () => <GlassUi />
