844e897c7348c83dcfbefd8c81d5ba3bee3b214d
Florian Bühler <florian-buehler@outlook.com>
2021-03-07 19:48:55 +0100

    chore: Add stylelint to lint css and scss files.

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+  extends: [
+    'stylelint-config-sass-guidelines', // https://github.com/bjankord/stylelint-config-sass-guidelines
+    'stylelint-config-idiomatic-order' // we want idiomatic ordering (not alphabetical ordering) https://github.com/ream88/stylelint-config-idiomatic-order
+  ],
+  rules: {
+    'indentation': 2,
+    'max-nesting-depth': 5,
+    'order/properties-alphabetical-order': null, // we do not want alphabetical ordering
+    'selector-class-pattern': null, // we do not want to enforce lowercase with hyphens, as this can lead to problems with CRA
+    'selector-max-compound-selectors': 5,
+    'selector-no-qualifying-type': null,
+    'shorthand-property-no-redundant-values': null
+  }
+}
