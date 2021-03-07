9993eb4660d5d27228db10eda0ad49512be95f08
Florian Bühler <florian-buehler@outlook.com>
2021-03-07 19:43:10 +0100

    feat: Add glass container.

--- src/components/GlassUi/index.tsx
+++ src/components/GlassUi/index.tsx
@@ -3,7 +3,7 @@ import classes from './styles.module.scss'
 
 const GlassUi: React.FC = (): React.ReactElement => {
   return (
-    <main className={classes.main}>
+    <main className={classes.container}>
       <section className={classes.glass}> </section>
       <div className={classes.circle1} />
       <div className={classes.circle2} />
