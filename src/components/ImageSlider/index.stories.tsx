fd3a294525944248246ae8114202dad629890380
Florian Bühler <florian-buehler@outlook.com>
2021-03-18 20:37:36 +0100

    chore: Add skeleton for the image slider.

---
+++ src/components/ImageSlider/index.stories.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import StoryBookLayout from 'components/StoryBookLayout'
+import ImageSlider from './index'
+
+export default {
+  title: 'Components/ImageSlider',
+  component: ImageSlider
+} as Meta
+
+export const ImageSliderStory: Story = () => (
+  <StoryBookLayout>
+    <ImageSlider />
+  </StoryBookLayout>
+)
