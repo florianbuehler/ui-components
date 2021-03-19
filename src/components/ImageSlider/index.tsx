feeaded6302a5ff840bb521d9300c49377238ae9
Florian Bühler <florian-buehler@outlook.com>
2021-03-19 19:54:54 +0100

    feat: Add html for image slider.

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,8 +1,21 @@
 ﻿import React from 'react'
 import classes from './styles.module.scss'
 
+import imgBefore from 'assets/images/imageSlider/landscape-grey.jpg'
+import imgAfter from 'assets/images/imageSlider/landscape-coloured.jpg'
+
 const ImageSlider: React.FC = (): React.ReactElement => {
-  return <main></main>
+  return (
+    <main className={classes.container}>
+      <div className={classes.imgContainerAfter}>
+        <img className={classes.imgAfter} src={imgAfter} alt="" />
+      </div>
+      <div className={classes.imgContainerBefore}>
+        <img className={classes.imgBefore} src={imgBefore} alt="" />
+      </div>
+      <div className={classes.slider} />
+    </main>
+  )
 }
 
 export default ImageSlider
