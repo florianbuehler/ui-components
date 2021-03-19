8f47e14d7c774b16fbd3b6040ad48e8cd90c54fd
Florian Bühler <florian-buehler@outlook.com>
2021-03-19 20:00:03 +0100

    chore: Add basic css reset to story book layout.

--- src/components/StoryBookLayout/index.tsx
+++ src/components/StoryBookLayout/index.tsx
@@ -1,5 +1,8 @@
 ﻿import React from 'react'
+
+// styles
 import classes from './styles.module.scss'
+import './styles.css'
 
 const StoryBookLayout: React.FC = ({ children }): React.ReactElement => {
   return <main className={classes.container}>{children}</main>
